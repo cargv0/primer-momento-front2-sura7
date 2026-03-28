f7a78ac33e86ff71f386be4445d5ca0dfc07fd48
cargv0 <cg34234@gmail.com>
2026-03-28 12:50:07 -0500

    re instalacion y arreglo de inconvenientes

---
+++ carlos-daniel-guzman-varela/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CourseCard from './components/CourseCard'
+import { cursos } from './data'
+
+function App() {
+ 
+  return (
+    <>
+     
+  )
+}
+
+export default App
