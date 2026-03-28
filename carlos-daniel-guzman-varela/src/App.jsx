76f679223bf6ce9cacf3c726204b8785a35b84cd
cargv0 <cg34234@gmail.com>
2026-03-28 13:23:38 -0500

    agregado contenido

--- carlos-daniel-guzman-varela/src/App.jsx
+++ carlos-daniel-guzman-varela/src/App.jsx
@@ -5,9 +5,26 @@ import { cursos } from './data'
 function App() {
  
   return (
-    <>
-     
-  )
+    <div className='main-container'>
+      <header className="header">
+        <h1 className='title'>Cursos<span>educativos</span></h1>
+        <p className='subtitle'>un espacio para aprender</p>
+      </header>
+      <main className="grid-cursos">
+        {cursos.map((curso, index) => (
+          <CourseCard 
+            key={curso.id} 
+            {...curso}
+            
+            index={index} 
+          />
+        ))}
+      </main>
+      <footer className='footer'>
+        <p> 2026 DevAcademy. Todos los derechos reservados.</p>
+        </footer>
+        </div>
+  );
 }
 
 export default App
