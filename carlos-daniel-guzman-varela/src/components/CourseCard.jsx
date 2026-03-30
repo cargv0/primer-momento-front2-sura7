ea12b27ce11688ebba8ac939ea782b2f2d981f9a
cargv0 <cg34234@gmail.com>
2026-03-29 19:30:59 -0500

    se cambia nombre de uno de los archivos y se agrega informacion requerida

---
+++ carlos-daniel-guzman-varela/src/components/CourseCard.jsx
@@ -0,0 +1,18 @@
+const CourseCard = ({ cursos }) => {
+    return (
+        <div>
+            <p>ID: {info.id}</p>
+            <h2>Titulo: {info.name}</h2>
+            <p>Contexto: {info.age}</p>
+            <p>Categoria: {info.email}</p>
+            <p>Intervalo: {info.phone}</p>
+            <p>Lecciones: {info.address}</p>
+            <p>Rango:</p>
+            <p>Precio:</p>
+            <p>Posicion:</p>
+            <p>Estudiante:</p>
+            <p>Informacion del curdo:</p>
+        </div>
+    )
+}
+export default CourseCard
